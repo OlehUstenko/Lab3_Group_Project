23350a1e530f75649fbda6dfb775cb657c6212b6
OlehUstenko <olehustenko7@gmail.com>
2026-04-10 22:58:14 +0300

    Додав метод виведення масиву

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,9 +2,28 @@
 {
     public partial class Program
     {
+        static int[] array;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool exit = false;
+            while (!exit)
+            {
+                //тут треба зробити меню виводу. Я в суботу зроблю
+
+            }
+        }
+        static void ArrayPrint(int[] array)
+        {
+            if (array.Length > 0)
+            {
+                for (int i = 0; i<array.Length; i++)
+                {
+                    Console.Write(array[i] + " ");
+                }
+                Console.WriteLine();
+            }
+            else Console.WriteLine("Масив порожній!");
+
         }
     }
 }
