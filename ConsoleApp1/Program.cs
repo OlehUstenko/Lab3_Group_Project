bc5e0472915c15769d91b08b67dd73a44f7f8caa
OlehUstenko <olehustenko7@gmail.com>
2026-04-10 23:35:04 +0300

    Додав меню вибору дії та переробив метод виведення масиву.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,29 +1,75 @@
-﻿namespace ConsoleApp1
+﻿using System.Text;
+
+namespace ConsoleApp1
 {
     public partial class Program
     {
         static int[] array;
         static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
             bool exit = false;
             while (!exit)
             {
-                //тут треба зробити меню виводу. Я в суботу зроблю
+                Console.WriteLine("ЛАБОРАТОРНА РОБОТА 3 (БЛОК 1)" +
+                    "\nПоточний стан масиву: " );
+                ArrayPrint(array);
+
+                Console.WriteLine("" +
+                    "\nОберіть дію" +
+                    "\n1. Заповнити масив вручну" +
+                    "\n2. Заповнити масив рандом" +
+                    "\n3. Виконати варіант Олега (Варіант 5)" +
+                    "\n4. Виконати варіант Євгена (Варіант 9)" +
+                    "\n5. Виконати варіант Серафима (Варіант 2)" +
+                    "\n0. Вихід" +
+                    "\nОберіть дію: ");
+                string choise = Console.ReadLine();
+
+                switch (choise)
+                {
+                    case "1":
+                        //метод заповнення вручну
+                        break;
+                    case "2":
+                        //метод заповнення рандомом
+                        break;
+                    case "3":
+                        //метод варіанту
+                        break;
+                    case "4":
+                        //метод варіанту
+                        break;
+                    case "5":
+                        //метод Варіанту
+                        break;
+                    case "0":
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Помилка! Не знайдено такого варіанту!\n");
+                        break;
+
+
+
+                }
+
+
 
             }
         }
         static void ArrayPrint(int[] array)
         {
-            if (array.Length > 0)
+            if (array == null)
             {
-                for (int i = 0; i<array.Length; i++)
-                {
-                    Console.Write(array[i] + " ");
-                }
-                Console.WriteLine();
+                Console.Write("Масив порожній!\n");
+                return;
             }
-            else Console.WriteLine("Масив порожній!");
-
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i].ToString() + " ");
+            }
+            Console.WriteLine();
         }
     }
 }
