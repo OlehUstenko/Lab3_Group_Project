836b792426132bb6518d3f9cb41ac9d8f87beee4
artemklocko432-tech <artemklocko432@gmail.com>
2026-04-21 21:25:53 +0300

    Create Variant_Klochko.cs

---
+++ ConsoleApp1/Variant_Klochko.cs
@@ -0,0 +1,40 @@
+namespace ConsoleApp4
+{
+    public partial class Program
+    {
+        public static void Variant4(ref int[] arr)
+        {
+            if (arr == null)
+            {
+                Console.WriteLine("Масив порожній!");
+                return;
+            }
+            Console.Write("Введіть К: ");
+            int k = int.Parse(Console.ReadLine());
+            Console.Write("Введіть Т: ");
+            int t = int.Parse(Console.ReadLine());
+            if (k < 0 || t < 0 || k + t > arr.Length)
+            {
+                Console.WriteLine("Дія неможлива: не всі елементи існують у масиві.");
+                return;
+            }
+            if (t == 0)
+            {
+                Console.WriteLine("T = 0, масив незмінний.");
+                return;
+            }
+            int[] newArr = new int[arr.Length - t];
+
+            int j = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i < k || i >= k + t)
+                {
+                    newArr[j] = arr[i];
+                    j++;
+                }
+            }
+            arr = newArr;
+        }
+    }
+}
