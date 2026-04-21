11bba9c567f0d5f342171c083326392cebff8793
sashkoboze-cmyk <sashkoboze@gmail.com>
2026-04-21 19:31:21 +0300

    Update Variant_Boze.cs

--- ConsoleApp1/Variant_Boze.cs
+++ ConsoleApp1/Variant_Boze.cs
@@ -1,76 +1,52 @@
-using System;
-
-class Program
+namespace ConsoleApp1
 {
-  static void Main()
+  public partial class Program
   {
-    int[] arr = InputArray();
+    static void Variant5(ref int[] arr)
+    {
+      if (arr == null)
+      {
+        Console.WriteLine("Масив порожній!");
+        return;
+      }
 
-    Console.Write("Введіть К: ");
-    int k = int.Parse(Console.ReadLine());
+      Console.Write("Введіть К: ");
+      int k = int.Parse(Console.ReadLine());
 
-    Console.Write("Введіть Т: ");
-    int t = int.Parse(Console.ReadLine());
+      Console.Write("Введіть Т: ");
+      int t = int.Parse(Console.ReadLine());
 
-    DeleteElements(ref arr, k, t);
+      if (k < 0)
+      {
+        Console.WriteLine("K < 0, нічого не робимо");
+        return;
+      }
 
-    Console.WriteLine("Результат:");
-    PrintArray(arr);
-}
+      if (k >= arr.Length)
+      {
+        Console.WriteLine("Початок за межами масиву");
+        return;
+      }
 
-static int[] InputArray()
-{
-  Console.Write("Кількість елементів: ");
-  int n = int.Parse(Console.ReadLine());
+      int realT = t;
+      if (k + t > arr.Length)
+        realT = arr.Length - k;
 
-  int[] arr = new int[n];
-  
-  for (int i = 0; i < n; i++)
-  {
-    Console.Write("arr[" + i + "] = ");
-    arr[i] = int.Parse(Console.ReadLine());
-  }
+      int[] newArr = new int[arr.Length - realT];
+      
+      int j = 0;
 
-  return arr;
-}
-
-static void PrintArray(int[] arr)
-{
-  for (int i = 0; i < arr.Length; i++)
-  Console.Write(arr[i] + " ");
-  Console.WriteLine();
-}
-
-static void DeleteElements(ref int[] arr, int k, int t)
-{
-  if (k < 0)
-  {
-    Console.WriteLine("K < 0, нічого не робимо");
-    return;
-  }
-
-if (k >= arr.Length)
-  {
-    Console.WriteLine("Початок за межами масиву");
-    return;
-  }
-
-int realT = t;
-if (k + t > arr.Length)
-    realT = arr.Length - k;
-
-int[] newArr = new int[arr.Length - realT];
-
-int j = 0;
-
-for (int i = 0; i < arr.Length; i++)
-{
-  if (i < k || i >= k + realT)
-  {
-      newArr[j] = arr[i];
-      j++;
+      for (int i = 0; i < arr.Length; i++)
+      {
+        if (i < k || i >= k + realT)
+        {
+          newArr[j] = arr[i];
+          j++;
+        }
+      }
+      arr = newArr;
+      }
   }
 }
-arr = newArr;
-}
-}
+      
+        
